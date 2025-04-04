1013cecf3256ecffe866de35ee69b2985b97847c
Frankam487 <fkamgang06@gmail.com>
2025-04-04 14:53:12 -0700

    first

--- gestion_taches/delete_task.php
+++ gestion_taches/delete_task.php
@@ -11,20 +11,3 @@ $stmt = $pdo->prepare("DELETE FROM taches WHERE id = :id" . (isAdmin() ? "" : " 
 $stmt->execute(isAdmin() ? ['id' => $id] : ['id' => $id, 'user_id' => $_SESSION['user_id']]);
 header('Location: index.php');
 exit;
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-
-<body>
-
-  <script src="./frank.js"></script>
-</body>
-
-</html>
--- gestion_taches/index.php
+++ gestion_taches/index.php
@@ -21,6 +21,7 @@ $taches = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Taches</title>
+  <script src="./frank.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 
